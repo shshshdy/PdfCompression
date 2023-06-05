940487f36cf8a6eff52a73dd634a62300f06c914
ssd <shshshdy@163.com>
2023-06-05 12:47:38 +0800

    异常处理

--- PdfCompression/MainWindow.axaml.cs
+++ PdfCompression/MainWindow.axaml.cs
@@ -5,6 +5,7 @@ using Avalonia.Rendering;
 using Avalonia.Threading;
 using DynamicData;
 using PdfCompression.pdf;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -38,41 +39,60 @@ namespace PdfCompression
                 _ = CompressPdf(pdf);
             };
         }
-
+        string _fileName = string.Empty;
+        bool _compressing = false;
         private async Task CompressPdf(string pdf)
         {
+            if (_compressing) return;
+            _compressing = true;
             await Task.Run(() =>
             {
-                var fileName = Path.GetFileName(pdf);
-                _vm.Name = fileName;
+                _fileName = Path.GetFileName(pdf);
+                _vm.Name = _fileName;
                 PdfHelper.Quality = _vm.Quality;
-                var outPdf = pdf.Replace(fileName, string.Empty);
-                outPdf += "new-" + fileName;
-                using (var source = new FileStream(pdf, FileMode.Open, FileAccess.Read, FileShare.Read))
-                using (var outFile = new FileStream(outPdf, FileMode.Create, FileAccess.Write, FileShare.ReadWrite))
-                using (var target = new MemoryStream())
+                var outPdf = pdf.Replace(_fileName, string.Empty);
+                outPdf += _fileName.ToLower().Replace(".pdf", "") + ".new.pdf";
+                try
                 {
-                    if (_vm.Loss)
+                    using (var source = new FileStream(pdf, FileMode.Open, FileAccess.Read, FileShare.Read))
+                    using (var outFile = new FileStream(outPdf, FileMode.Create, FileAccess.Write, FileShare.ReadWrite))
+                    using (var target = new MemoryStream())
                     {
-                        PdfHelper.Convert(source, target, (per) => _ = UpdatePer(per));
-                        if (source.Length > target.Length)
+                        if (_vm.Loss)
                         {
-                            outFile.Write(target.ToArray());
-                            return;
+                            PdfHelper.Convert(source, target, (per) => _ = UpdatePer(per));
+                            if (source.Length > target.Length)
+                            {
+                                outFile.Write(target.ToArray());
+                                return;
+                            }
+                            target.SetLength(0);
                         }
-                        target.SetLength(0);
-                    }
-                    PdfHelper.Convert2(source, target, _vm.Loss, (per) => _ = UpdatePer(per));
+                        PdfHelper.Convert2(source, target, _vm.Loss, (per) => _ = UpdatePer(per));
 
-                    if (target.Length > source.Length)
-                    {
-                        var bytes = new byte[source.Length];
-                        source.Seek(0, SeekOrigin.Begin);
-                        source.Read(bytes, 0, bytes.Length);
-                        outFile.Write(bytes);
+                        if (target.Length > source.Length)
+                        {
+                            var bytes = new byte[source.Length];
+                            source.Seek(0, SeekOrigin.Begin);
+                            source.Read(bytes, 0, bytes.Length);
+                            outFile.Write(bytes);
+                        }
+                        else
+                            outFile.Write(target.ToArray());
                     }
-                    else
-                        outFile.Write(target.ToArray());
+                    
+                }
+                catch (IOException)
+                {
+                    _vm.Name = "文件被其它程序占用或磁盘空间不足！";
+                }
+                catch (Exception ex)
+                {
+                    _vm.Name = "压缩失败！参考提示：" + ex.Message;
+                }
+                finally
+                {
+                    _compressing = false;
                 }
 
             });
@@ -83,7 +103,7 @@ namespace PdfCompression
             _vm.Per = per * 100;
             if (_vm.Per >= 100)
             {
-                _vm.Name += "，压缩完成！";
+                _vm.Name = _fileName + "，压缩完成！";
             }
             await Task.Delay(10);
         }
